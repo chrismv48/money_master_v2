ee73a372d710da925584717fe46cd352be07b572
Chris Armstrong <chris.r.armstrong@gmail.com>
2019-08-21 18:58:50 -0400

    fix email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
     port: 587,
     user_name: Rails.application.credentials.gmail[:username],
     password: Rails.application.credentials.gmail[:password],
