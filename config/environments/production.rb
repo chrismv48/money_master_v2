fa4f94a5fee42d809b2736b710ce7771e4dff892
Chris Armstrong <chris.r.armstrong@gmail.com>
2019-08-21 19:47:17 -0400

    update gmail username, fix smtp settings (again)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,12 +79,15 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
+    domain: "heroku.com",
     port: 587,
     user_name: Rails.application.credentials.gmail[:username],
     password: Rails.application.credentials.gmail[:password],
     authentication: :plain,
     enable_starttls_auto: true
   }
+
+  config.action_mailer.default_url_options = { host: 'https://serene-island-25311.herokuapp.com'}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
