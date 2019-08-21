e345838d7a6d09859f7d9aa2aae8cd6285c33e54
Chris Armstrong <chris.r.armstrong@gmail.com>
2019-08-21 18:48:09 -0400

    fix email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,12 +78,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :user_name => Rails.application.credentials.gmail[:username],
-    :password => Rails.application.credentials.gmail[:password],
-    :authentication => :plain,
-    :enable_starttls_auto => true
+    port: 587,
+    user_name: Rails.application.credentials.gmail[:username],
+    password: Rails.application.credentials.gmail[:password],
+    authentication: :plain,
+    enable_starttls_auto: true
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
